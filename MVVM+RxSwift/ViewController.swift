22402117fddb58c842ba182132dc5b17979a896c
Alterego11 <alterego15071970@gmail.com>
2021-10-19 19:58:22 +0300

    Create Project

---
+++ MVVM+RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM+RxSwift
+//
+//  Created by Антон Белый on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
